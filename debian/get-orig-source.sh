f2f94f0f8027552031567d1352ef997badfcc47e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-10-29 00:27:54 +0200

    Use new location

--- debian/get-orig-source.sh
+++ debian/get-orig-source.sh
@@ -11,21 +11,21 @@ hg clone https://bitbucket.org/syntext/serna-free-4-4
 GZIP="--best --no-name" hg archive -R serna-free-4-4 -r $REV -p serna-free-$VERSION serna-free_$VERSION.orig.tar.gz
 
 cd debian/3rd
-wget -c http://downloads.syntext.com/serna/opensource/3rd/DITA-OT1.4_fullpackage_bin.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/PyQt-x11-gpl-4.5.4.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/Python-2.6.2.tar.bz2
-wget -c http://downloads.syntext.com/serna/opensource/3rd/QScintilla-gpl-2.4.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/antlr-2.7.1.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/aspell-0.50.5.tar.bz2
-wget -c http://downloads.syntext.com/serna/opensource/3rd/docbook-xsl-1.68.1.tar.bz2
-wget -c http://downloads.syntext.com/serna/opensource/3rd/doxygen-1.5.1.src.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/jdk-linux-1.6.0.tar.bz2
-wget -c http://downloads.syntext.com/serna/opensource/3rd/libiconv-1.11.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/libxml2-2.6.27.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/libxslt-1.1.19.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/qt-all-opensource-src-4.5.3.tar.bz2
-wget -c http://downloads.syntext.com/serna/opensource/3rd/qtpropertybrowser-2.5-opensource.tar.gz
-wget -c http://downloads.syntext.com/serna/opensource/3rd/sip-4.8.2.tar.gz
-wget -O hunspell-bin-1.3.tgz -c http://sourceforge.net/projects/hunspell/files/Hunspell/1.3.2/hunspell-1.3.2.tar.gz
-wget -c http://ftp.heanet.ie/mirrors/ftp.trolltech.com/pub/qt/solutions/lgpl/qtsingleapplication-2.6_1-opensource.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/DITA-OT1.4_fullpackage_bin.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/PyQt-x11-gpl-4.5.4.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/Python-2.6.2.tar.bz2
+wget -c http://downloads.syntext.com/serna-free-3rd/QScintilla-gpl-2.4.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/antlr-2.7.1.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/aspell-0.50.5.tar.bz2
+wget -c http://downloads.syntext.com/serna-free-3rd/docbook-xsl-1.68.1.tar.bz2
+wget -c http://downloads.syntext.com/serna-free-3rd/doxygen-1.5.1.src.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/jdk-linux-1.6.0.tar.bz2
+wget -c http://downloads.syntext.com/serna-free-3rd/libiconv-1.11.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/libxml2-2.6.27.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/libxslt-1.1.19.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/qt-all-opensource-src-4.5.3.tar.bz2
+wget -c http://downloads.syntext.com/serna-free-3rd/qtpropertybrowser-2.5-opensource.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/sip-4.8.2.tar.gz
+wget -c http://downloads.syntext.com/serna-free-3rd/hunspell-bin-1.3.tgz
+wget -c http://downloads.syntext.com/serna-free-3rd/qtsingleapplication-2.6_1-opensource.tar.gz
 cd ../..
